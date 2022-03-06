45532fae2cbe8d594e074be27ea8dbe3231198fa
Guilherme Saraiva Bluhm <80696919+guilhermebluhm@users.noreply.github.com>
2022-03-06 16:12:17 -0300

    update misc permissoesLogin

--- src/main/kotlin/com/kot/model/contaAdmin.kt
+++ src/main/kotlin/com/kot/model/contaAdmin.kt
@@ -1,10 +1,14 @@
 package com.kot.model
 
 import concederPermissao
+import revogarPermissoes
 
 open abstract class contaAdmin(var nomePessoa: String, val identFuncionario: Int,
                                var enderecoFuncionario: enderecoCliente, val flagLogin : Boolean) {
     fun getPermissoesLogin() : Boolean{
         return concederPermissao()
+    }
+    fun revogarPermissaoLogin() : Boolean{
+        return revogarPermissoes()
     }
 }
